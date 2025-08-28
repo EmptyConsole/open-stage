3420ff4c56d2c7e0f680f749b47a0145bce963e4
ConsoleEmpty <consoleempty@gmail.com>
2025-08-27 21:28:19 -0700

    Basic Shaayer page. More will be added

---
+++ src/app/shaayer/page.js
@@ -0,0 +1,9 @@
+import SpecialButton from "@/components/SpecialButton";
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className="ShyGuy">Shaayer</h1>
+    </main>
+  );
+}
