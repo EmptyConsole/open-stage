2e0453a5386e90a12bbe7f3da60c84c9ccafeff8
ConsoleEmpty <shyguygamedev@gmail.com>
2025-08-27 22:02:09 -0700

    button code

--- src/app/shaayer/page.js
+++ src/app/shaayer/page.js
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <main>
       <h1 className="ShyGuy">Shaayer</h1>
+      <button className="ShyGuyButton" id="buttonOnes">"Click me for caszsh"</button>
     </main>
   );
 }
