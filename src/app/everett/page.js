d063d1cc470fd0ba7c391364b4d35e31bdcd4e3b
ConsoleEmpty <shyguygamedev@gmail.com>
2025-08-29 16:29:08 -0700

    Update page.js

--- src/app/everett/page.js
+++ src/app/everett/page.js
@@ -1,9 +1,11 @@
 // import "./page.module.css";
+import Sidebar from "@/components/sidebar.js";
 export default function HomePage() {
   return (
     <main>
-      <Sidebar />
       <h1>Welcome to Everetts page</h1>
+      <Sidebar />
+      
     </main>
   );
 }
