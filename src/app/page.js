51f61067bf470cb991bde2402b5eab68eb65aaef
ConsoleEmpty <shyguygamedev@gmail.com>
2025-09-03 22:29:52 -0700

    kinda crappy sidebar and header for OpenStage in globals and homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,17 @@ import styles from "./page.module.css";
 import "./globals.css";
 export default function HomePage() {
   return (
-    <main style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", height: "100vh" }}>
-      <header className="header-container">
-        <div className="header-bar"></div>
-      </header>
-    </main>
+    
+    <body className="main">
+      <aside className="sidebar">
+        <div style={{padding: "65px 16px", color: "#222" }}>Sidebar content goes here</div>
+      </aside>
+      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <header className="header-container"> 
+          <div className="header-bar"></div>
+        </header>
+        {/* Main content goes here */}
+      </div>
+    </body>
   );
 }
