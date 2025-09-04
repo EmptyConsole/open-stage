fa32a63c4807d4a794448fcf0b6630d808ea163c
Fang Hai Sen <hansonfang1012@gmail.com>
2025-09-03 21:55:40 -0700

    Change

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,11 @@
 import styles from "./page.module.css";
-
+import "./globals.css";
 export default function HomePage() {
   return (
-    <main>
-      <h1>This is my test homepage</h1>
-      <div className={styles.sidebar}>
-        {/* Sidebar to the left */}
-        <div className={styles.elements}>
-          <h1>SIDEBAR</h1>
-          <h1>SIDEBAR</h1>
-          <h1>SIDEBAR</h1>
-          <h1>SIDEBAR</h1>
-        </div>
-        {/* Body content to the right */}
-        <div className={styles.elementsButBetter}>
-          <h1>Welcome to my homepage. This is some placeholder body text.</h1>
-        </div>
-      </div>
+    <main style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", height: "100vh" }}>
+      <header className="header-container">
+        <div className="header-bar"></div>
+      </header>
     </main>
   );
 }
