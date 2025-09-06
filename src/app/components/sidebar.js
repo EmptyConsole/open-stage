993f57a01d8e16582a4a044bfdb42c5c046aff4c
LWCoding <DiamondMob01@gmail.com>
2025-09-05 21:09:55 -0500

    Pushed children example to showcase nesting components inside of each other

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -1,18 +1,25 @@
-export default function Sidebar({ content }) {
+export default function Sidebar({ children }) {
   return (
-    <aside style={{
-      width: '330px',
-      height: '100vh',
-      backgroundColor: '#e0e0e0',
-      boxShadow: '2px 0 5px rgba(0,0,0,0.05)',
-      position: 'relative'
-    }}>
+    <aside
+      style={{
+        width: "330px",
+        height: "100vh",
+        backgroundColor: "#e0e0e0",
+        boxShadow: "2px 0 5px rgba(0,0,0,0.05)",
+        position: "relative",
+      }}
+    >
       <div style={{ padding: "65px 0px" }}>
-        <div style={{textAlign: "center",
+        <div
+          style={{
+            textAlign: "center",
             fontSize: "20px",
             textJustify: "center",
-            color: "black"
-        }}>{content}</div>
+            color: "black",
+          }}
+        >
+          {children}
+        </div>
       </div>
     </aside>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,21 @@ import "./globals.css";
 import Sidebar from "./components/sidebar";
 export default function HomePage() {
   return (
-    
     <main className="main">
-      <Sidebar content={"Hello, world!"}/>
+      <Sidebar>
+        <div>
+          <p>Lucas is cool!</p>
+          <h1>Hello!</h1>
+          <button>Click Me</button>
+        </div>
+      </Sidebar>
       <div className="header_main">
-        <header className="header-container"> 
+        <header className="header-container">
           <div className="header-bar">
             <div>Header content goes here</div>
           </div>
-            <div className="main-content">Main content goes here</div>
+          <div className="main-content">Main content goes here</div>
         </header>
-          
       </div>
     </main>
   );
