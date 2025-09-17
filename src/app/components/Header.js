1a3ce74150d730abc74a9f6a317bb936964eb080
toff-tofu <e8623y@gmail.com>
2025-09-16 20:24:52 -0700

    Update header with navbar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -19,9 +19,28 @@ export default function Header() {
       <Link href="/" style={{ color: "white", textDecoration: "none", fontSize: "30px", fontWeight: "bold" }}>
         OpenStage
       </Link>
-      <Link href="/viewtickets" style={{ color: "white", textDecoration: "none", fontSize: "18px", fontWeight: "bold" }}>
-        View Tickets
-      </Link>
+      <div style={{
+        // padding: "16px 32px",
+        color: "white",
+        backgroundColor: "rgba(31, 70, 117, 1)",
+        fontSize: "30px",
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "30%",
+        // boxSizing: "border-box",
+      }}>
+        <Link href="/viewtickets" style={{ color: "white", textDecoration: "none", fontSize: "18px", fontWeight: "bold" }}>
+          View Tickets
+        </Link>
+        <Link href="/localconcertmap" style={{ color: "white", textDecoration: "none", fontSize: "18px", fontWeight: "bold" }}>
+          Nearby Concerts
+        </Link>
+        <Link href="/aboutus" style={{ color: "white", textDecoration: "none", fontSize: "18px", fontWeight: "bold" }}>
+          About Us
+        </Link>
+      </div>
     </div>
   );
 }
